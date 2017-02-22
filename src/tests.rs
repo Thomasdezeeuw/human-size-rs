aeab10800ab3fe40d11a815bb71557fb869e1869
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2017-02-22 17:03:54 +0100

    fix formatting of tests
    Tabs are better, however Rust uses 4 spaces…

--- src/tests.rs
+++ src/tests.rs
@@ -3,7 +3,7 @@ use super::*;
 #[test]
 fn size_try_into_u32() {
     let tests = vec![
-		(Size::new(1, Multiple::Byte), Ok(1)),
+        (Size::new(1, Multiple::Byte), Ok(1)),
 
         (Size::new(1, Multiple::Kilobyte), Ok(1_000)),
         (Size::new(23, Multiple::Kilobyte), Ok(23_000)),
@@ -28,7 +28,7 @@ fn size_try_into_u32() {
 #[test]
 fn size_try_into_u64() {
     let tests = vec![
-		(Size::new(1, Multiple::Byte), Ok(1)),
+        (Size::new(1, Multiple::Byte), Ok(1)),
 
         (Size::new(1, Multiple::Kilobyte), Ok(1_000)),
         (Size::new(23, Multiple::Kilobyte), Ok(23_000)),
@@ -54,7 +54,7 @@ fn size_try_into_u64() {
 #[test]
 fn size_try_into_u128() {
     let tests = vec![
-		(Size::new(1, Multiple::Byte), Ok(1)),
+        (Size::new(1, Multiple::Byte), Ok(1)),
 
         (Size::new(1, Multiple::Kilobyte), Ok(1_000)),
         (Size::new(23, Multiple::Kilobyte), Ok(23_000)),
@@ -85,26 +85,26 @@ fn size_try_into_u128() {
 #[test]
 fn size_from_str() {
     let tests = vec![
-		("100 B", Ok(Size::new(100, Multiple::Byte))),
+        ("100 B", Ok(Size::new(100, Multiple::Byte))),
 
-		("12 kB", Ok(Size::new(12, Multiple::Kilobyte))),
-		("25 MB", Ok(Size::new(25, Multiple::Megabyte))),
-		("1 GB", Ok(Size::new(1, Multiple::Gigabyte))),
-		("1000 TB", Ok(Size::new(1000, Multiple::Terabyte))),
-		("12 PB", Ok(Size::new(12, Multiple::Petabyte))),
-		("10 EB", Ok(Size::new(10, Multiple::Exabyte))),
-		("12 ZB", Ok(Size::new(12, Multiple::Zettabyte))),
-		("0 YB", Ok(Size::new(0, Multiple::Yottabyte))),
+        ("12 kB", Ok(Size::new(12, Multiple::Kilobyte))),
+        ("25 MB", Ok(Size::new(25, Multiple::Megabyte))),
+        ("1 GB", Ok(Size::new(1, Multiple::Gigabyte))),
+        ("1000 TB", Ok(Size::new(1000, Multiple::Terabyte))),
+        ("12 PB", Ok(Size::new(12, Multiple::Petabyte))),
+        ("10 EB", Ok(Size::new(10, Multiple::Exabyte))),
+        ("12 ZB", Ok(Size::new(12, Multiple::Zettabyte))),
+        ("0 YB", Ok(Size::new(0, Multiple::Yottabyte))),
 
-		("99999 KB", Ok(Size::new(99999, Multiple::Kibibyte))),
-		("1 KiB", Ok(Size::new(1, Multiple::Kibibyte))),
-		("12 MiB", Ok(Size::new(12, Multiple::Mebibyte))),
-		("123 GiB", Ok(Size::new(123, Multiple::Gigibyte))),
-		("129 TiB", Ok(Size::new(129, Multiple::Tebibyte))),
-		("99 PiB", Ok(Size::new(99, Multiple::Pebibyte))),
-		("45 EiB", Ok(Size::new(45, Multiple::Exbibyte))),
-		("12 ZiB", Ok(Size::new(12, Multiple::Zebibyte))),
-		("2 YiB", Ok(Size::new(2, Multiple::Yobibyte))),
+        ("99999 KB", Ok(Size::new(99999, Multiple::Kibibyte))),
+        ("1 KiB", Ok(Size::new(1, Multiple::Kibibyte))),
+        ("12 MiB", Ok(Size::new(12, Multiple::Mebibyte))),
+        ("123 GiB", Ok(Size::new(123, Multiple::Gigibyte))),
+        ("129 TiB", Ok(Size::new(129, Multiple::Tebibyte))),
+        ("99 PiB", Ok(Size::new(99, Multiple::Pebibyte))),
+        ("45 EiB", Ok(Size::new(45, Multiple::Exbibyte))),
+        ("12 ZiB", Ok(Size::new(12, Multiple::Zebibyte))),
+        ("2 YiB", Ok(Size::new(2, Multiple::Yobibyte))),
 
         ("", Err(ParsingError::NoValue)),
         ("10 abc", Err(ParsingError::UnknownMultiple)),
@@ -120,17 +120,17 @@ fn size_from_str() {
 #[test]
 fn size_to_string() {
     let tests = vec![
-		(Size::new(100, Multiple::Byte), "100 B"),
+        (Size::new(100, Multiple::Byte), "100 B"),
 
-		(Size::new(2, Multiple::Kilobyte), "2 kB"),
-		(Size::new(25, Multiple::Megabyte), "25 MB"),
-		(Size::new(3, Multiple::Gigabyte), "3 GB"),
-		(Size::new(38, Multiple::Terabyte), "38 TB"),
-		(Size::new(100, Multiple::Zettabyte), "100 ZB"),
+        (Size::new(2, Multiple::Kilobyte), "2 kB"),
+        (Size::new(25, Multiple::Megabyte), "25 MB"),
+        (Size::new(3, Multiple::Gigabyte), "3 GB"),
+        (Size::new(38, Multiple::Terabyte), "38 TB"),
+        (Size::new(100, Multiple::Zettabyte), "100 ZB"),
 
-		(Size::new(2, Multiple::Mebibyte), "2 MiB"),
-		(Size::new(3, Multiple::Zebibyte), "3 ZiB"),
-		(Size::new(1000, Multiple::Yobibyte), "1000 YiB"),
+        (Size::new(2, Multiple::Mebibyte), "2 MiB"),
+        (Size::new(3, Multiple::Zebibyte), "3 ZiB"),
+        (Size::new(1000, Multiple::Yobibyte), "1000 YiB"),
     ];
 
     for test in tests {
@@ -143,15 +143,15 @@ fn size_to_string() {
 #[test]
 fn multiple_try_into_u32() {
     let tests = vec![
-		(Multiple::Byte, Ok(1)),
+        (Multiple::Byte, Ok(1)),
 
-		(Multiple::Kilobyte, Ok(1_000)),
-		(Multiple::Megabyte, Ok(1_000_000)),
-		(Multiple::Gigabyte, Ok(1_000_000_000)),
+        (Multiple::Kilobyte, Ok(1_000)),
+        (Multiple::Megabyte, Ok(1_000_000)),
+        (Multiple::Gigabyte, Ok(1_000_000_000)),
 
-		(Multiple::Kibibyte, Ok(1024)),
-		(Multiple::Mebibyte, Ok(1_048_576)),
-		(Multiple::Gigibyte, Ok(1_073_741_824)),
+        (Multiple::Kibibyte, Ok(1024)),
+        (Multiple::Mebibyte, Ok(1_048_576)),
+        (Multiple::Gigibyte, Ok(1_073_741_824)),
 
         (Multiple::Terabyte, Err(ConversionError::Overflow)),
     ];
@@ -166,19 +166,19 @@ fn multiple_try_into_u32() {
 #[test]
 fn multiple_try_into_u64() {
     let tests = vec![
-		(Multiple::Byte, Ok(1)),
+        (Multiple::Byte, Ok(1)),
 
-		(Multiple::Kilobyte, Ok(1_000)),
-		(Multiple::Megabyte, Ok(1_000_000)),
-		(Multiple::Gigabyte, Ok(1_000_000_000)),
-		(Multiple::Terabyte, Ok(1_000_000_000_000)),
-		(Multiple::Petabyte, Ok(1_000_000_000_000_000)),
+        (Multiple::Kilobyte, Ok(1_000)),
+        (Multiple::Megabyte, Ok(1_000_000)),
+        (Multiple::Gigabyte, Ok(1_000_000_000)),
+        (Multiple::Terabyte, Ok(1_000_000_000_000)),
+        (Multiple::Petabyte, Ok(1_000_000_000_000_000)),
 
-		(Multiple::Kibibyte, Ok(1024)),
-		(Multiple::Mebibyte, Ok(1_048_576)),
-		(Multiple::Gigibyte, Ok(1_073_741_824)),
-		(Multiple::Tebibyte, Ok(1_099_511_627_776)),
-		(Multiple::Pebibyte, Ok(1_125_899_906_842_624)),
+        (Multiple::Kibibyte, Ok(1024)),
+        (Multiple::Mebibyte, Ok(1_048_576)),
+        (Multiple::Gigibyte, Ok(1_073_741_824)),
+        (Multiple::Tebibyte, Ok(1_099_511_627_776)),
+        (Multiple::Pebibyte, Ok(1_125_899_906_842_624)),
 
         (Multiple::Exabyte, Err(ConversionError::Overflow)),
     ];
@@ -193,22 +193,22 @@ fn multiple_try_into_u64() {
 #[test]
 fn multiple_try_into_u128() {
     let tests = vec![
-		(Multiple::Byte, Ok(1)),
+        (Multiple::Byte, Ok(1)),
 
-		(Multiple::Kilobyte, Ok(1_000)),
-		(Multiple::Megabyte, Ok(1_000_000)),
-		(Multiple::Gigabyte, Ok(1_000_000_000)),
-		(Multiple::Terabyte, Ok(1_000_000_000_000)),
-		(Multiple::Petabyte, Ok(1_000_000_000_000_000)),
+        (Multiple::Kilobyte, Ok(1_000)),
+        (Multiple::Megabyte, Ok(1_000_000)),
+        (Multiple::Gigabyte, Ok(1_000_000_000)),
+        (Multiple::Terabyte, Ok(1_000_000_000_000)),
+        (Multiple::Petabyte, Ok(1_000_000_000_000_000)),
         (Multiple::Exabyte, Ok(1_000_000_000_000_000_000)),
         (Multiple::Zettabyte, Ok(1_000_000_000_000_000_000_000)),
         (Multiple::Yottabyte, Ok(1_000_000_000_000_000_000_000_000)),
 
-		(Multiple::Kibibyte, Ok(1024)),
-		(Multiple::Mebibyte, Ok(1_048_576)),
-		(Multiple::Gigibyte, Ok(1_073_741_824)),
-		(Multiple::Tebibyte, Ok(1_099_511_627_776)),
-		(Multiple::Pebibyte, Ok(1_125_899_906_842_624)),
+        (Multiple::Kibibyte, Ok(1024)),
+        (Multiple::Mebibyte, Ok(1_048_576)),
+        (Multiple::Gigibyte, Ok(1_073_741_824)),
+        (Multiple::Tebibyte, Ok(1_099_511_627_776)),
+        (Multiple::Pebibyte, Ok(1_125_899_906_842_624)),
         (Multiple::Exbibyte, Ok(1_152_921_504_606_846_976)),
         (Multiple::Zebibyte, Ok(1_180_591_620_717_411_303_424)),
         (Multiple::Yobibyte, Ok(1_208_925_819_614_629_174_706_176)),
@@ -224,26 +224,26 @@ fn multiple_try_into_u128() {
 #[test]
 fn multiple_from_str() {
     let tests = vec![
-		("B", Ok(Multiple::Byte)),
+        ("B", Ok(Multiple::Byte)),
 
-		("kB", Ok(Multiple::Kilobyte)),
-		("MB", Ok(Multiple::Megabyte)),
-		("GB", Ok(Multiple::Gigabyte)),
-		("TB", Ok(Multiple::Terabyte)),
-		("PB", Ok(Multiple::Petabyte)),
-		("EB", Ok(Multiple::Exabyte)),
-		("ZB", Ok(Multiple::Zettabyte)),
-		("YB", Ok(Multiple::Yottabyte)),
+        ("kB", Ok(Multiple::Kilobyte)),
+        ("MB", Ok(Multiple::Megabyte)),
+        ("GB", Ok(Multiple::Gigabyte)),
+        ("TB", Ok(Multiple::Terabyte)),
+        ("PB", Ok(Multiple::Petabyte)),
+        ("EB", Ok(Multiple::Exabyte)),
+        ("ZB", Ok(Multiple::Zettabyte)),
+        ("YB", Ok(Multiple::Yottabyte)),
 
-		("KB", Ok(Multiple::Kibibyte)),
-		("KiB", Ok(Multiple::Kibibyte)),
-		("MiB", Ok(Multiple::Mebibyte)),
-		("GiB", Ok(Multiple::Gigibyte)),
-		("TiB", Ok(Multiple::Tebibyte)),
-		("PiB", Ok(Multiple::Pebibyte)),
-		("EiB", Ok(Multiple::Exbibyte)),
-		("ZiB", Ok(Multiple::Zebibyte)),
-		("YiB", Ok(Multiple::Yobibyte)),
+        ("KB", Ok(Multiple::Kibibyte)),
+        ("KiB", Ok(Multiple::Kibibyte)),
+        ("MiB", Ok(Multiple::Mebibyte)),
+        ("GiB", Ok(Multiple::Gigibyte)),
+        ("TiB", Ok(Multiple::Tebibyte)),
+        ("PiB", Ok(Multiple::Pebibyte)),
+        ("EiB", Ok(Multiple::Exbibyte)),
+        ("ZiB", Ok(Multiple::Zebibyte)),
+        ("YiB", Ok(Multiple::Yobibyte)),
 
         ("", Err(ParsingError::UnknownMultiple)),
         ("abc", Err(ParsingError::UnknownMultiple)),
@@ -259,25 +259,25 @@ fn multiple_from_str() {
 #[test]
 fn multiple_to_string() {
     let tests = vec![
-		(Multiple::Byte, "B"),
+        (Multiple::Byte, "B"),
 
-		(Multiple::Kilobyte, "kB"),
-		(Multiple::Megabyte, "MB"),
-		(Multiple::Gigabyte, "GB"),
-		(Multiple::Terabyte, "TB"),
-		(Multiple::Petabyte, "PB"),
-		(Multiple::Exabyte, "EB"),
-		(Multiple::Zettabyte, "ZB"),
-		(Multiple::Yottabyte, "YB"),
+        (Multiple::Kilobyte, "kB"),
+        (Multiple::Megabyte, "MB"),
+        (Multiple::Gigabyte, "GB"),
+        (Multiple::Terabyte, "TB"),
+        (Multiple::Petabyte, "PB"),
+        (Multiple::Exabyte, "EB"),
+        (Multiple::Zettabyte, "ZB"),
+        (Multiple::Yottabyte, "YB"),
 
-		(Multiple::Kibibyte, "KiB"),
-		(Multiple::Mebibyte, "MiB"),
-		(Multiple::Gigibyte, "GiB"),
-		(Multiple::Tebibyte, "TiB"),
-		(Multiple::Pebibyte, "PiB"),
-		(Multiple::Exbibyte, "EiB"),
-		(Multiple::Zebibyte, "ZiB"),
-		(Multiple::Yobibyte, "YiB"),
+        (Multiple::Kibibyte, "KiB"),
+        (Multiple::Mebibyte, "MiB"),
+        (Multiple::Gigibyte, "GiB"),
+        (Multiple::Tebibyte, "TiB"),
+        (Multiple::Pebibyte, "PiB"),
+        (Multiple::Exbibyte, "EiB"),
+        (Multiple::Zebibyte, "ZiB"),
+        (Multiple::Yobibyte, "YiB"),
     ];
 
     for test in tests {
